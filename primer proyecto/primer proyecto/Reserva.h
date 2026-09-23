246ddfe8dc739206029a70e4056bd528e44f0328
CarlosLSA <carlos.solis.alonso@est.una.ac.cr>
2026-09-23 16:03:10 -0600

    Refactor Reserva class to include new attributes

--- primer proyecto/primer proyecto/Reserva.h
+++ primer proyecto/primer proyecto/Reserva.h
@@ -1,33 +1,36 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include"Cancha.h"
-#include"Cliente.h"
-#include"ColeccionCanchas.h"
-#include"ColeccionClientes.h"
+#include "Cancha.h"
+#include "Cliente.h"
 
 using namespace std;
 
-class Reserva
-{
+class Reserva {
 private:
-	int numeroReserva;
-	Cliente* cliente;
-	Cancha* cancha;
-	float monto;
-	bool activa;
+    static int contadorConsecutivo; // Para generar el id autoincremental de forma global
+    int numeroReserva;
+    Cliente* cliente;
+    Cancha* cancha;
+    int franjaInicial;     
+    int cantidadFranjas;     
+    float monto;
+    bool activa;
 
 public:
     Reserva();
-    Reserva(Cliente* clientePtr, Cancha* canchaPtr, float monto);
+    Reserva(Cliente* clientePtr, Cancha* canchaPtr, int franjaInicial, int cantidadFranjas, float monto);
 
     // Getters
     int getIdReserva() const;
     Cliente* getCliente() const;
     Cancha* getCancha() const;
+    int getFranjaInicial() const;
+    int getCantidadFranjas() const;
     float getMonto() const;
     bool isActiva() const;
+
+    // Métodos de comportamiento
     void cancelar();
     void mostrarInformacion() const;
 };
-
