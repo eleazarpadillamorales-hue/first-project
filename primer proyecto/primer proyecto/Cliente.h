702fb1bf47e3b43a9c10b93b962406c83dfd872a
CarlosLSA <carlos.solis.alonso@est.una.ac.cr>
2026-09-23 22:32:03 -0600

    Refactor Cliente class comments and mostrar method
    Updated comments and method signature for mostrar() to be const.

--- primer proyecto/primer proyecto/Cliente.h
+++ primer proyecto/primer proyecto/Cliente.h
@@ -7,24 +7,24 @@ using namespace std;
 
 class Cliente {
 private:
-    // Atributos privados
     string id;
     string nombre;
     string telefono;
 
 public:
-    // Constructor por defecto
     Cliente();
-    // Constructor con parámetros
     Cliente(string id, string nombre, string telefono);
-    //Destructor
     ~Cliente();
-    // Métodos Getters (Consultores)
+
+    // Getters (Consultores)
     string getId() const;
     string getNombre() const;
     string getTelefono() const;
-    // Métodos Setters (Modificadores)
+
+    // Setters (Modificadores)
     void setNombre(string nombre);
     void setTelefono(string telefono);
-    void mostrar();
+
+    // Muestra de datos
+    void mostrar() const;
 };
